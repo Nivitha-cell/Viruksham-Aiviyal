dccfd29b5e1efdccc813acb84a8f25c77d1b3da6
unknown <venkatesannivitha@gmail.com>
2024-10-05 16:50:49 +0530

    Updated Flask app with sector selection and 405 error fix

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 import pandas as pd
 import os
@@ -9,19 +8,17 @@ app = Flask(__name__, template_folder='docs')
 data_path = os.path.join(os.getcwd(), 'data', 'Yojana info.csv')
 data = pd.read_csv(data_path)
 
-# Define sectors and schemes
+# Define sectors
 sectors = data['Sector'].unique()
-schemes = data[['Sector', 'Yojana Name', 'Eligibility', 'Deadline', 'Benefits']]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
         sector = request.form['sector']
-        filtered_schemes = schemes[schemes['Sector'] == sector]
-        return render_template('results.html', name=name, schemes=filtered_schemes.to_dict(orient='records'))
+        filtered_data = data[data['Sector'] == sector]
+        return render_template('results.html', name=name, data=filtered_data.to_dict(orient='records'))
     return render_template('index.html', sectors=sectors)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
